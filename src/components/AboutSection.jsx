7c8a8498ce886e8c187bded4e049916b5bcfa889
GarvitKhare07 <147191959+GarvitKhare07@users.noreply.github.com>
2025-12-10 19:28:21 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="Resume.pdf"
+                href="https://drive.google.com/file/d/1pkMcn-uc9dUgrd-BuyT10OpVDLibFb3i/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
